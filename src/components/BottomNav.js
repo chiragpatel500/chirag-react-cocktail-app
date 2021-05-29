3e6b49fe628620ba9c84c1a8a1c8c6ae2847adcc
chiragpatel500 <chirag12354@gmail.com>
2021-05-29 14:32:31 +0200

    update

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,8 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import LocationOnIcon from '@material-ui/icons/LocationOn';
 import history from '../history';
 
 
@@ -24,7 +24,7 @@ const useStyles = makeStyles({
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
 
   return (
     <BottomNavigation
